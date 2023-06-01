9cdcaa500740fe8a1ca46e7e456c951bfb2b80e6
kimara <goldknow456@gmail.com>
2023-06-01 13:22:29 +0900

    Add: [canvas] 01. 심플 예제
    - day1

--- canvas/01.simple/script.js
+++ canvas/01.simple/script.js
@@ -9,17 +9,52 @@ const canvasHeight = 300;
 canvas.style.width = `${canvasWidth}px`;
 canvas.style.height = `${canvasHeight}px`;
 
-canvas.width = canvasWidth * dpr; 
+canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-// ctx.fillRect(10, 10, 50, 50);
+//4)
+class Particle {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360); //3)
+        ctx.fillStyle = "yellow";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
 
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, (Math.PI / 180) * 360); //3)
+const x = 100;
+const y = 100;
+const radius = 50;
+const particle = new Particle(x, y, radius);
 
-ctx.fill();
-ctx.closePath();
+let interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
+
+function animate() {
+    window.requestAnimationFrame(animate); //6)
+    now = Date.now();
+    delta = now - then;
+
+    if (delta < interval) return;
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight); //5)
+    //x를 1px씩 이동시키기
+
+
+    particle.draw();
+
+    then = now - (delta % interval);
+}
+
+animate();
 
 /**
  * 1)
@@ -31,8 +66,25 @@ ctx.closePath();
  * Device-pixel-ratio(dpr): 하나의 css픽셀을 그릴 때 사용되는 장치의 픽셀 수
  * 1px 을 실제로 그리는데 몇 px을 쓰는지 알 수 있다.
  * dpr이 높을수록 선명한 그래픽을 보여줄 수 있다.
- * 
+ *
  * 3)
- * degree가 아닌 radian을 사용하기 때문에 지름 / 180 * 360이라고 적어야 한다. 
+ * degree가 아닌 radian을 사용하기 때문에 지름 / 180 * 360이라고 적어야 한다.
  * 각도 1도는 파이 3.14를 180으로 나눈 값과 같기 때문에 라디안을 단위로 쓰는 것에 각도를 쓰기 위해서는 이렇게 써야 한다.
+ *
+ * 4)
+ * Particle 클래스를 통해서 여러개의 원을 생성시켜주고 싶은데, 각각 다른 위치에 생성시켜주기 위해서는
+ * Particle 클래스의 constructor 안에서 정의된 x와 y값을 밖에서 각각 다르게 설정을 해주고 인스텃르를 생성해주게 되면
+ * 각각 다른 x와 y좌표를 가진 인스턴스를 만들 수 있다.
+ *
+ * 5)
+ * 매 프레임마다 전체화면을 지우고 다음 프레임에서 Particle을 draw한다.
+ *
+ * 6)
+ * requestAnimationFrame: 1초에 모니터 주사율에 맞춰서 실행된다.
+ * 모니터 주사율에 따라 애니메이션이 달라진다.
+ * FPS를 적용한다.
+ * 👉 내 모니터 주사율: 60hz = 1초에 60번 실행 = 약 16ms 마다 requestAnimationFrame 이 실행된다.
+ * 👉 내 애니메이션의 목표 fps = 10 = 1초에 10번 프레임을 찍어라 = 100ms마다 requestAnimationFrame 을 실행시킨다.
+ * if(delta > interval) 애니메이션 동작!
+ * then = now - (delta % interval)
  */
