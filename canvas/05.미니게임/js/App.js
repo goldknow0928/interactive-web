6f998e5f0525ef4d061380ea3e5fe6426a9fc33d
kimara <goldknow456@gmail.com>
2024-10-15 15:36:19 +0900

    Add: [canvas] 미니게임1

---
+++ canvas/05.미니게임/js/App.js
@@ -0,0 +1,41 @@
+export default class App {
+    static canvas = document.querySelector("canvas");
+    static ctx = App.canvas.getContext("2d");
+    static dpr = devicePixelRatio > 1 ? 2 : 1;
+    static interval = 1000 / 60;
+    static width = 1024;
+    static height = 768;
+
+    constructor() {
+        window.addEventListener("resize", this.resize.bind(this));
+    }
+
+    resize() {
+        App.canvas.width = App.width * App.dpr;
+        App.canvas.height = App.height * App.dpr;
+        App.ctx.scale(App.dpr, App.dpr);
+
+        const width = innerWidth > innerHeight ? innerHeight * 0.9 : innerWidth * 0.9;
+        App.canvas.style.width = width + "px";
+        App.canvas.style.height = width * (3 / 4) + "px";
+    }
+
+    render() {
+        let now, delta;
+        let then = Date.now();
+        const frame = () => {
+            requestAnimationFrame(frame);
+            now = Date.now;
+            delta = now - delta;
+
+            if (delta < App.interval) return;
+
+            App.ctx.clearRect(0, 0, App.width, App.height);
+            App.ctx.fillRect(50, 50, 100, 100);
+
+            then = now - (delta % App.interval);
+        };
+
+        requestAnimationFrame(frame);
+    }
+}
